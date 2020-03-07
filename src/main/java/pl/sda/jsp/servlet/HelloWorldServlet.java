d4e9345b24d637b24907bbed196b61164de46a60
Ela <elzbieta.maksym@gmail.com>
2020-03-07 10:20:31 +0100

    adnotacja

---
+++ src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package pl.sda.jsp.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+public class HelloWorldServlet extends HttpServlet {
+
+
+    @Override
+    public void  doGet(HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
+
+        final PrintWriter writer = response.getWriter();
+        writer.println("Hello world");
+    }
+}
